bc2bae837c71e8a0a2c5fef1db9dd9bd2eb7d498
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-03-25 20:02:19 +0000

    user profile page begun. getting info from database. needs more work

--- src/app/profile/[userId]/page.js
+++ src/app/profile/[userId]/page.js
@@ -1,19 +1,20 @@
 // user profile page
-import { getProfile, getPosts } from "@/utils/utils";
-import { notFound } from "next/navigation";
+import { getProfile } from "@/utils/utils";
+// import { notFound } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 import ViewProfile from "@/components/ViewProfile";
 
 export default async function Page({ params }) {
   const { userId } = auth();
   const userProfile = await getProfile(params.userId);
+  console.log(userId);
 
   if (!userProfile) {
-    notFound();
+    // notFound();
   }
   return (
     <div>
-      <ViewProfile />
+      <ViewProfile profile={userProfile} />
       <p>users home profile page will be here.</p>
     </div>
   );
--- src/components/ViewProfile.jsx
+++ src/components/ViewProfile.jsx
@@ -3,8 +3,9 @@ import { getProfile } from "@/utils/utils";
 export default async function ViewProfile({ profile }) {
   return (
     <div>
-      <p>{profile.firstName}</p>
-      <p>{profile.lastName}</p>
+      <p>{profile.first_name}</p>
+      <p>{profile.last_name}</p>
+      <p>{profile.user_name}</p>
     </div>
   );
 }
